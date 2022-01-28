71a5ce4c90cd9db9f46c91868cc882ec6fa65e30
Andrew Walbran <qwandor@google.com>
2022-01-28 20:49:30 +0000

    Read events from files.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 mod model;
 
-use crate::model::{
-    event::{DanceStyle, Event},
-    events::Events,
-};
+use crate::model::events::Events;
 use eyre::Report;
 
 fn main() -> Result<(), Report> {
@@ -11,43 +8,7 @@ fn main() -> Result<(), Report> {
     pretty_env_logger::init();
     color_backtrace::install();
 
-    let events = Events {
-        events: vec![
-            Event {
-                name: "London Barndance with Mark Elvins and English Contra Dance Band".to_string(),
-                details: Some(
-                    "Contra dance with Mark Elvins calling and the English Contra Dance Band playing.".to_string(),
-                ),
-                links: vec![
-                    "https://www.barndance.org/programme.html".to_string(),
-                    "https://www.facebook.com/events/243417821143957/".to_string(),
-                ],
-                country: "UK".to_string(),
-                city: "London".to_string(),
-                styles: vec![DanceStyle::Contra],
-                workshop: false,
-                social: true,
-                bands: vec!["English Contra Dance Band".to_string()],
-                callers: vec!["Mark Elvins".to_string()],
-                price: Some("£5-£14".to_string()),
-                organisation: Some("London Barndance Company".to_string()),
-            },
-            Event {
-                name: "Minimal".to_string(),
-                details: None,
-                links: vec![],
-                country: "UK".to_string(),
-                city: "London".to_string(),
-                styles: vec![],
-                workshop: false,
-                social: false,
-                bands: vec![],
-                callers: vec![],
-                price: None,
-                organisation: None,
-            },
-        ],
-    };
+    let events = Events::load()?;
 
     for event in &events.events {
         println!("{:?}", event);
--- src/model/events.rs
+++ src/model/events.rs
@@ -1,8 +1,39 @@
 use super::event::Event;
+use eyre::{bail, Report, WrapErr};
+use log::trace;
 use serde::{Deserialize, Serialize};
+use std::fs::{read_dir, read_to_string};
+
+const EVENTS_DIRECTORY: &str = "events";
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct Events {
     pub events: Vec<Event>,
+}
+
+impl Events {
+    pub fn load() -> Result<Self, Report> {
+        let mut events = vec![];
+        for entry in read_dir(EVENTS_DIRECTORY)? {
+            let filename = entry?.path();
+            trace!("Reading events from {:?}", filename);
+            let contents =
+                read_to_string(&filename).wrap_err_with(|| format!("Reading {:?}", filename))?;
+            let file_events = toml::from_str::<Events>(&contents)?.events;
+            for event in &file_events {
+                let problems = event.validate();
+                if !problems.is_empty() {
+                    bail!(
+                        "Problems with event '{}' in {:?}: {:?}",
+                        event.name,
+                        filename,
+                        problems
+                    );
+                }
+            }
+            events.extend(file_events);
+        }
+        Ok(Self { events })
+    }
 }
