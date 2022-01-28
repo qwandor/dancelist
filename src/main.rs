8430213681099eb63f28090f5a0502ff16455023
Andrew Walbran <qwandor@google.com>
2022-01-28 03:04:03 +0000

    Set up error reporting, backtraces and logging.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 mod model;
 
-fn main() {}
+use eyre::Report;
+
+fn main() -> Result<(), Report> {
+    stable_eyre::install()?;
+    pretty_env_logger::init();
+    color_backtrace::install();
+
+    Ok(())
+}
