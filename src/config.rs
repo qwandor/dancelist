68961d986fe5779002e854306fa272425dab0613
Andrew Walbran <qwandor@google.com>
2022-01-28 21:46:16 +0000

    Read bind address from config file.

---
+++ src/config.rs
@@ -0,0 +1,65 @@
+use eyre::{bail, Report, WrapErr};
+use serde::Deserialize;
+use std::{
+    fs::read_to_string,
+    net::SocketAddr,
+    path::{Path, PathBuf},
+};
+
+/// Paths at which to look for the config file. They are searched in order, and the first one that
+/// exists is used.
+const CONFIG_FILENAMES: [&str; 2] = ["dancelist.toml", "/etc/dancelist.toml"];
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Config {
+    #[serde(default = "default_public_dir")]
+    pub public_dir: PathBuf,
+    #[serde(default = "default_bind_address")]
+    pub bind_address: SocketAddr,
+}
+
+impl Config {
+    pub fn from_file() -> Result<Config, Report> {
+        for filename in &CONFIG_FILENAMES {
+            if Path::new(filename).is_file() {
+                return Config::read(filename);
+            }
+        }
+        bail!(
+            "Unable to find config file in any of {:?}",
+            &CONFIG_FILENAMES
+        );
+    }
+
+    fn read(filename: &str) -> Result<Config, Report> {
+        let config_file =
+            read_to_string(filename).wrap_err_with(|| format!("Reading {}", filename))?;
+        Ok(toml::from_str(&config_file)?)
+    }
+}
+
+fn default_public_dir() -> PathBuf {
+    Path::new("public").to_path_buf()
+}
+
+fn default_bind_address() -> SocketAddr {
+    "0.0.0.0:3000".parse().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Parsing the example config file should not give any errors.
+    #[test]
+    fn example_config() {
+        Config::read("dancelist.example.toml").unwrap();
+    }
+
+    /// Parsing an empty config file should not give any errors.
+    #[test]
+    fn empty_config() {
+        toml::from_str::<Config>("").unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
+mod config;
 mod controllers;
 mod errors;
 mod model;
 
-use crate::controllers::index;
-use crate::model::events::Events;
+use crate::{config::Config, controllers::index, model::events::Events};
 use axum::{routing::get, AddExtensionLayer, Router};
 use eyre::Report;
 use log::info;
-
-const BIND_ADDRESS: &str = "127.0.0.1:1234";
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
@@ -16,15 +14,15 @@ async fn main() -> Result<(), Report> {
     pretty_env_logger::init();
     color_backtrace::install();
 
+    let config = Config::from_file()?;
     let events = Events::load()?;
 
     let app = Router::new()
         .route("/", get(index))
         .layer(AddExtensionLayer::new(events));
 
-    let bind_address = BIND_ADDRESS.parse()?;
-    info!("Listening on {}", bind_address);
-    axum::Server::bind(&bind_address)
+    info!("Listening on {}", config.bind_address);
+    axum::Server::bind(&config.bind_address)
         .serve(app.into_make_service())
         .await?;
 
