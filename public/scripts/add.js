fbf11dc502305b04c120cd175dffa13cb038c6eb
Andrew Walbran <qwandor@google.com>
2022-12-18 21:10:56 +0000

    Copy data list of inputs too
    Otherwise only the first band and caller is auto-completed.

--- public/scripts/add.js
+++ public/scripts/add.js
@@ -1,8 +1,9 @@
-function add_input(list, name, type, id) {
+function add_input(list, name, type, id, datalist) {
   var input = document.createElement("input");
   input.setAttribute("type", type);
   input.setAttribute("name", name);
   input.setAttribute("id", id);
+  input.setAttribute("list", datalist);
   var li = document.createElement("li");
   li.appendChild(input);
   list.appendChild(li);
@@ -17,7 +18,7 @@ function initialise() {
     var last_input = this.lastElementChild.lastElementChild;
     if (last_input.value != "") {
       // Last element has been entered, add a new one.
-      add_input(this, last_input.name, last_input.type, last_input.id);
+      add_input(this, last_input.name, last_input.type, last_input.id, last_input.list.id);
       last_input.id = null;
     } else if (second_to_last_input.value == "") {
       // Last two elements are empty, remove one.
