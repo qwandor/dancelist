ae8983d9d18fd8efd9d25d5001561d5755c10027
Andrew Walbran <qwandor@google.com>
2023-01-14 15:05:12 +0000

    Move function out of other function.

--- public/scripts/add.js
+++ public/scripts/add.js
@@ -9,24 +9,30 @@ function add_input(list, name, type, id, datalist) {
   list.appendChild(li);
 }
 
-function initialise() {
-  function update_inputs() {
-    if (this.children.length > 1) {
-      var second_to_last_input =
-        this.children[this.children.length - 2].lastElementChild;
-    }
-    var last_input = this.lastElementChild.lastElementChild;
-    if (last_input.value != "") {
-      // Last element has been entered, add a new one.
-      add_input(this, last_input.name, last_input.type, last_input.id, last_input.list.id);
-      last_input.id = null;
-    } else if (second_to_last_input != null && second_to_last_input.value == "") {
-      // Last two elements are empty, remove one.
-      second_to_last_input.id = last_input.id;
-      this.lastElementChild.remove();
-    }
+function update_inputs() {
+  if (this.children.length > 1) {
+    var second_to_last_input =
+      this.children[this.children.length - 2].lastElementChild;
   }
+  var last_input = this.lastElementChild.lastElementChild;
+  if (last_input.value != "") {
+    // Last element has been entered, add a new one.
+    add_input(
+      this,
+      last_input.name,
+      last_input.type,
+      last_input.id,
+      last_input.list.id
+    );
+    last_input.id = null;
+  } else if (second_to_last_input != null && second_to_last_input.value == "") {
+    // Last two elements are empty, remove one.
+    second_to_last_input.id = last_input.id;
+    this.lastElementChild.remove();
+  }
+}
 
+function initialise() {
   document.getElementById("links_list").oninput = update_inputs;
   document.getElementById("bands_list").oninput = update_inputs;
   document.getElementById("callers_list").oninput = update_inputs;
