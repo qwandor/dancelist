010ace5dd1ecc2dba4772f47ff52b7cac63a129a
Andrew Walbran <qwandor@google.com>
2024-10-07 14:52:41 +0000

    Detect more bands.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 339] = [
+pub const BANDS: [&str; 341] = [
     "112 and Then Some",
     "A Fine Kettle of Fish",
     "A Joyful Noise",
@@ -81,6 +81,7 @@ pub const BANDS: [&str; 339] = [
     "Ciac Boum",
     "Ciona",
     "Cojiro",
+    "Constellation",
     "Contra Banditos",
     "Contra Intuitive",
     "ContraForce",
@@ -247,6 +248,7 @@ pub const BANDS: [&str; 339] = [
     "Ormuz",
     "Pamplemousse",
     "Paracetamol",
+    "Parasol",
     "Persons of Quality",
     "PFM!",
     "Philu",
