3500b6a508498e0204d973ec1ebc5f60fc5fdfe2
Andrew Walbran <qwandor@google.com>
2024-10-14 20:05:32 +0100

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 342] = [
+pub const BANDS: [&str; 343] = [
     "112 and Then Some",
     "A Fine Kettle of Fish",
     "A Joyful Noise",
@@ -152,6 +152,7 @@ pub const BANDS: [&str; 342] = [
     "Geronimo",
     "Gisbert",
     "Go Figure",
+    "Good Company",
     "Good Intentions",
     "Gott Folk!",
     "Göze",
