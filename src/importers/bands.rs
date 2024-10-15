8779643966bcb0250d8d0b2cceb8b20d2210bc76
Andrew Walbran <qwandor@google.com>
2024-10-15 18:40:50 +0100

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 347] = [
+pub const BANDS: [&str; 348] = [
     "112 and Then Some",
     "A Fine Kettle of Fish",
     "A Joyful Noise",
@@ -60,6 +60,7 @@ pub const BANDS: [&str; 347] = [
     "Biskaya",
     "Blind Squirrel",
     "BmB",
+    "Boccadoro",
     "Bougnat Sound",
     "Bourrée Party Crackers",
     "Bouton",
