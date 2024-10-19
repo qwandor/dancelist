d41649d063a5c9b761f29ff5a0e54d32db280aee
Andrew Walbran <qwandor@google.com>
2024-10-19 17:03:05 +0000

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 350] = [
+pub const BANDS: [&str; 351] = [
     "112 and Then Some",
     "A Fine Kettle of Fish",
     "A Joyful Noise",
@@ -150,6 +150,7 @@ pub const BANDS: [&str; 350] = [
     "Folkschwestern Biese",
     "Fourpence",
     "French Toast",
+    "Froghammer",
     "Full Tilt",
     "Fyndus",
     "George Paul",
