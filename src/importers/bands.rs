9dd6180784464c63c5a9e5a27d3885dd54cde7f5
Andrew Walbran <qwandor@google.com>
2024-10-24 13:57:12 +0100

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 353] = [
+pub const BANDS: [&str; 354] = [
     "112 and Then Some",
     "A Fine Kettle of Fish",
     "A Joyful Noise",
@@ -46,6 +46,7 @@ pub const BANDS: [&str; 353] = [
     "Ballkanesen",
     "Ballo Allegro",
     "Bamako Express",
+    "Bandage a Trois",
     "Bare Necessities",
     "Bargainatt",
     "Barock op de Deel",
