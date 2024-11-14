fd8d9e754486da19cc9f27d160b064ba488ff7e6
Andrew Walbran <qwandor@google.com>
2024-11-14 19:21:40 +0000

    Detect more socials.

--- src/importers/icalendar/balfolknl.rs
+++ src/importers/icalendar/balfolknl.rs
@@ -67,7 +67,7 @@ impl IcalendarSource for BalfolkNl {
             || parts.summary.starts_with("Balfolk in Kleve")
             || parts.summary.starts_with("Balfolk met ")
             || parts.summary.starts_with("BalFolk met ")
-            || parts.summary.starts_with("Balfolk op zondagmiddag")
+            || parts.summary.starts_with("Balfolk op zondag")
             || parts.summary.starts_with("Balfolk op de")
             || parts.summary.starts_with("Balfolkcafe")
             || parts.summary.starts_with("BresBal")
