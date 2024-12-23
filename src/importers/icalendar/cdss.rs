faebc643b2638be403b9ee7e698e42fddf0bd88c
Andrew Walbran <qwandor@google.com>
2024-12-23 12:00:02 +0000

    Detect more workshops.

--- src/importers/icalendar/cdss.rs
+++ src/importers/icalendar/cdss.rs
@@ -44,6 +44,7 @@ impl IcalendarSource for Cdss {
             || description_lower.contains("basics session")
             || description_lower.contains("basics/review session")
             || description_lower.contains("beginner introduction")
+            || description_lower.contains("beginner class")
             || description_lower.contains("beginner session")
             || description_lower.contains("beginner teaching")
             || description_lower.contains("beginner workshop")
