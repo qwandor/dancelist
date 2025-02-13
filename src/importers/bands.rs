486262d0c2423bc5daa9239ca7cdd9feec35ce8c
Andrew Walbran <qwandor@google.com>
2025-02-13 12:55:39 +0000

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 429] = [
+pub const BANDS: [&str; 430] = [
     "112 and Then Some",
     "A Fine Kettle of Fish",
     "A Joyful Noise",
@@ -193,6 +193,7 @@ pub const BANDS: [&str; 429] = [
     "Gray & Friends",
     "GrayScale",
     "Gribouille",
+    "Griff Trio",
     "Guillaume Sparrow-Pepin & Rachel Bell",
     "Harken!",
     "Hartwin Dhoore Trio",
