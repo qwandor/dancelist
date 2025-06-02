fe333d48a991767dba06e792eac40d3f16731e20
Andrew Walbran <qwandor@google.com>
2025-06-02 12:27:49 +0100

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 497] = [
+pub const BANDS: [&str; 498] = [
     "112 and Then Some",
     "A Cat's Tale",
     "A Fine Kettle of Fish",
@@ -240,6 +240,7 @@ pub const BANDS: [&str; 497] = [
     "Irish Diplomacy",
     "Javallon",
     "Jeroen Laureyssens",
+    "Jet Bridge",
     "Joachim Montbord",
     "Joanna Hyde & Tadhg O Meachair",
     "Jormsons Kapell",
