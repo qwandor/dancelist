3db1cf4556cc88f63af6611b2c4a3301f39372c5
Andrew Walbran <qwandor@google.com>
2025-08-26 16:12:39 +0100

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 516] = [
+pub const BANDS: [&str; 517] = [
     "112 and Then Some",
     "A Cat's Tale",
     "A Fine Kettle of Fish",
@@ -67,6 +67,7 @@ pub const BANDS: [&str; 516] = [
     "Ben Bolker & Susanne Maziarz",
     "Benjamin Rowe & Adam Rich-Griffin",
     "Berkenwerk",
+    "Big Brandy",
     "Big Fun",
     "Biskaya",
     "Black Cat Quadrille",
