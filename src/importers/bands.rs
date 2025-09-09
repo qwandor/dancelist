0f16397cbd6989349b1b2857ec960f5628427b89
Andrew Walbran <qwandor@google.com>
2025-09-09 13:56:21 +0100

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 523] = [
+pub const BANDS: [&str; 524] = [
     "112 and Then Some",
     "A Cat's Tale",
     "A Fine Kettle of Fish",
@@ -413,6 +413,7 @@ pub const BANDS: [&str; 523] = [
     "Starling",
     "StatiK",
     "Steam!",
+    "Stefano Baldan",
     "Steppin' Stones",
     "Stepwise",
     "Stomp Rocket",
