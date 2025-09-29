b74b4cd152467cd1f46c60e0692c759978e47862
Andrew Walbran <qwandor@google.com>
2025-09-29 17:53:31 +0100

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 534] = [
+pub const BANDS: [&str; 535] = [
     "112 and Then Some",
     "A Cat's Tale",
     "A Fine Kettle of Fish",
@@ -436,6 +436,7 @@ pub const BANDS: [&str; 534] = [
     "Swinco",
     "Swingology",
     "Sycamore",
+    "Tabbies Delight",
     "Take a Dance",
     "Tea & Whiskey",
     "Thalas",
