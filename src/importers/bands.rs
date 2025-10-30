374b1bde79bb3a24e262758083bd383c2056b390
Andrew Walbran <qwandor@google.com>
2025-10-30 11:57:48 +0000

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 548] = [
+pub const BANDS: [&str; 549] = [
     "112 and Then Some",
     "A Cat's Tale",
     "A Fine Kettle of Fish",
@@ -280,6 +280,7 @@ pub const BANDS: [&str; 548] = [
     "Kingfisher",
     "Klintetten",
     "Kördeböf",
+    "Koriander",
     "KV Express",
     "Kvinnene",
     "L'air Inconnu",
