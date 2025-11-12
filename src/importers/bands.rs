919b56ef5c8b3ea5a2265c1d511978e237a9d15b
Andrew Walbran <qwandor@google.com>
2025-11-12 11:58:50 +0000

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 555] = [
+pub const BANDS: [&str; 556] = [
     "112 and Then Some",
     "A Cat's Tale",
     "A Fine Kettle of Fish",
@@ -454,6 +454,7 @@ pub const BANDS: [&str; 555] = [
     "Sycamore",
     "Tabbies Delight",
     "Take a Dance",
+    "Taranteira",
     "Tea & Whiskey",
     "Thalas",
     "The 32 Bartenders",
