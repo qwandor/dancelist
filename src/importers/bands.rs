8a0263b76afa1f4880ebf6dcb23ab9484ba2d172
Andrew Walbran <qwandor@google.com>
2025-11-28 21:59:56 +0000

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 566] = [
+pub const BANDS: [&str; 567] = [
     "112 and Then Some",
     "A Cat's Tale",
     "A Fine Kettle of Fish",
@@ -339,6 +339,7 @@ pub const BANDS: [&str; 566] = [
     "Martina & Gisbert & Rainer",
     "Matt Norman & Edward Wallace",
     "Matt Norman & Hannah Cummings",
+    "Matt Norman",
     "Mavro Meli",
     "Meadowhawk",
     "Merriment",
