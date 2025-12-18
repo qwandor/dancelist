93eb25d48eaa5f3738df0ac29429e2ff8f875d29
Andrew Walbran <qwandor@google.com>
2025-12-18 12:33:46 +0000

    Detect another band.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 574] = [
+pub const BANDS: [&str; 575] = [
     "112 and Then Some",
     "A Cat's Tale",
     "A Fine Kettle of Fish",
@@ -384,6 +384,7 @@ pub const BANDS: [&str; 574] = [
     "Nozzy",
     "Nubia",
     "Nut en Genoegen",
+    "Odevare",
     "Oh CONTRAire",
     "Old Time All Stars",
     "Olivier Valence",
