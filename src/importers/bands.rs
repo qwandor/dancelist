6def21464ddb7bc255b4a8e61e4aaefe80554027
Andrew Walbran <qwandor@google.com>
2026-03-15 17:25:52 +0000

    Fix build.

--- src/importers/bands.rs
+++ src/importers/bands.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const BANDS: [&str; 623] = [
+pub const BANDS: [&str; 624] = [
     "112 and Then Some",
     "A Cat's Tale",
     "A Fine Kettle of Fish",
